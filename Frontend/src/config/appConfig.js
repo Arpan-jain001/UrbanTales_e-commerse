c4138e89ba7e1e516351c90100a79cc972827fb0
Arpan-jain001 <arpanjain00123@gmail.com>
2026-04-12 19:15:28 +0530

    update

--- Frontend/src/config/appConfig.js
+++ Frontend/src/config/appConfig.js
@@ -9,12 +9,12 @@ export const API_BASE_URL = readEnv("VITE_BACKEND_API_URL", "http://localhost:30
 );
 
 export const FIREBASE_CONFIG = {
-  apiKey: readEnv("VITE_SELLER_FIREBASE_APIKEY"),
-  authDomain: readEnv("VITE_SELLER_FIREBASE_AUTH_DOMAIN", "urbantales-seller.firebaseapp.com"),
-  projectId: readEnv("VITE_SELLER_FIREBASE_PROJECT_ID", "urbantales-seller"),
-  storageBucket: readEnv("VITE_SELLER_FIREBASE_STORAGE_BUCKET", "urbantales-seller.appspot.com"),
-  messagingSenderId: readEnv("VITE_SELLER_FIREBASE_MESSAGING_SENDER_ID", "630373689851"),
-  appId: readEnv("VITE_SELLER_FIREBASE_APP_ID", "1:630373689851:web:d7f8b8f15c8dbe619f835e"),
+  apiKey: readEnv("VITE_FIREBASE_APIKEY"),
+  authDomain: readEnv("VITE_FIREBASE_AUTH_DOMAIN",),
+  projectId: readEnv("VITE_FIREBASE_PROJECT_ID"),
+  storageBucket: readEnv("VITE_FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: readEnv("VITE_FIREBASE_MESSAGING_SENDER_ID"),
+  appId: readEnv("VITE_FIREBASE_APP_ID"),
 };
 
 export const SELLER_FIREBASE_CONFIG = {
